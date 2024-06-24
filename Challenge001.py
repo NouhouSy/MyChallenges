2f25482d565365ac8f11a90251ae1d784ec3d91a
NouhouSy <159595118+NouhouSy@users.noreply.github.com>
2024-06-24 18:18:41 +0000

    second commit

--- Challenge001.py
+++ Challenge001.py
@@ -1,2 +1,14 @@
-#1000TeachLeader Challenge
-print("Hello, 1000Teach")
+#'1000 TeachLeaders python programming
+First_name = "Ibrahima Nouhou"
+Last_name = "Sy"
+Full_name = "Sy Ibrahima Nouhou"
+Country = "Guinée"
+City = "Conakry"
+Age = 23
+Year = 2000
+Is_married = True
+Is_true = "no"
+Is_light_on = "my city"
+nom, Prenom, age = "Diallo", "Malick", 23
+
+print(First_name)
